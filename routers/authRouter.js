327daa1f5e3e016c623d19ae2066d3286a4972dd
AlexSouza <alexsouzadev@gmail.com>
2025-05-07 12:40:23 +0100

    Controller and Router fix

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const authController = require('../controllers/authController'); // Importing auth controller
+const router = express.Router(); // Create a new router instance
 
-const router = express.Router();
 
-router.post('/register', (req, res) => {
-    res.json({ message: 'Register' });
-});
+router.post('/register', authController.register); // Route for user registration
 
 module.exports = router;
